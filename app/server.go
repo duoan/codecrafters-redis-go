a43b2a86632caa0a610d1df8a5d9dd73c2a218c3
Victor An <duoan@amazon.com>
2023-01-18 16:38:19 -0800

    pass 2st stage

--- app/server.go
+++ app/server.go
@@ -19,9 +19,35 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		// Listen for an incoming connection
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		// Handle connections in a new goroutine
+		go func(conn net.Conn) {
+			// close when function ends
+			defer conn.Close()
+
+			buf := make([]byte, 1024)
+			len, err := conn.Read(buf)
+
+			if err != nil {
+				fmt.Printf("Error reading: %#v\n", err)
+			}
+
+			request := string(buf[:len])
+			fmt.Printf("Message received: %s\n\n", request)
+
+			_, err = conn.Write([]byte("PONG\r\n"))
+			if err != nil {
+				fmt.Printf("Error Writing: %#v\n", err)
+			}
+		}(conn)
 	}
+
 }
